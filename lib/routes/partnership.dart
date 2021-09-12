8c72d877a0ae2345da798693f7b75c6c6722146a
Hamza BENJEDDI <hamza.benjeddi@ynov.com>
2021-09-12 17:58:24 +0100

    update

---
+++ lib/routes/partnership.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Partnership extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('partnerships'),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    ),
+  );
+}
